ae64926f6027e609df3b9f143928252557158ead
kelton117 <89748860+kelton117@users.noreply.github.com>
2021-09-21 15:53:13 -0500

    intial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(function(){
+
+    
+    const BASE_URL = 'http://www.omdbapi.com/';
+    const API_KEY = '83848083';
+    let movieData;
+    
+    
+    const $title = $('#title'); 
+    const $plot = $('#plot');
+    const $year = $('#year');
+    const $director = $('#director')
+    const $genre = $('#genre')
+    const $rated = $('#rated');
+    const $form = $('form'); 
+    const $input = $('input[type="text"]');
+    const $img = $('img');
+
+    
+    $form.on('submit', handleGetData)
+    
+    
+    
+    
+    function handleGetData(event) {
+        
+        event.preventDefault(); 
+       
+        const movieTitle = $input.val();
+        $input.val("");
+        
+        $.ajax(`${BASE_URL}?apikey=${API_KEY}&t=${movieTitle}`).then(function (data) {
+           
+            movieData = data; 
+            render();
+          
+            
+        }, function (error) {
+           
+            console.log(error);
+        });
+
+       
+
+    }
+    
+    
+    function render() {
+        $title.text(movieData.Title);
+        $plot.text(movieData.Plot);
+        $year.text(movieData.Year);
+        $rated.text(movieData.Rated);
+        $director.text(movieData.Director);
+        $genre.text(movieData.Genre);
+        $('body').css("background-image", `url(${movieData.Poster})`);
+    }
+    
+        
+    });
